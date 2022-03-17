b53be0da160d3058de3862722a30d13070e42c71
Harshil-bit <75490038+Harshil-bit@users.noreply.github.com>
2022-03-17 13:07:42 +0530

    Update Rating.py

--- Rating.py
+++ Rating.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.figure_factory as ff
 
-df=pd.read_csv('py/P-108.csv')
+df=pd.read_csv('P-108.csv')
 avg_rating_list=df["Avg Rating"].to_list()
 fig=ff.create_distplot([avg_rating_list],["Average Rating"],show_hist=False)
 fig.show()
